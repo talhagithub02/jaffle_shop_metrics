a624b917346adeb63064d636b6f019d1ba6f955a
talha.iftikhar@alexsolutions.com.au <talha.iftikhar@alexsolutions.com.au>
2026-07-27 10:39:12 +0000

    dsd

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,4 +1,4 @@
-{% set results = run_query('select 1 as id') %}
+{% set results = run_query('select 1 as id')%}
 {{ config(alias='orderStaging',schema='dbt_test') }}
 
 with source as (
@@ -7,7 +7,7 @@ with source as (
     Normally we would select from the table here, but we are using seeds to load
     our data in this project
     #}
-    select * from {{ source('public','orderssssss') }}
+    select * from {{ source('public','orderssss') }}
 
 ),
 source_2 as (
@@ -16,7 +16,7 @@ source_2 as (
     Normally we would select from the table here, but we are using seeds to load
     our data in this project
     #}
-    select * from {{ source('public','customer') }}
+    select * from 
 
 ),
 source_3 as (
@@ -37,7 +37,7 @@ renamed as (
         id as order_id,
         user_id as customer_id,
         order_date,
-        status
+        status,,,
 
     from source
 	where order_date in ('{{ var("start_date") }}')
